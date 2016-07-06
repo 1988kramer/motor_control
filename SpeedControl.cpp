17301bddf4b586b6e14130dfca83ae03178f7beb
Andrew Kramer <1988kramer@gmail.com>
2016-07-06 15:28:58 -0700

    Added documentation to SpeedControl.cpp

--- SpeedControl.cpp
+++ SpeedControl.cpp
@@ -11,7 +11,10 @@
 #include<Encoder.h>
 
 #define DEFAULT_MIN_SPEED 60
+const double defaultGain = 1.0;
 
+// accepts a pointer to a Motor and a pointer to an Encoder as parameters
+// sets gains to 1.0 and minimum speed to 60 by default
 SpeedControl::SpeedControl(Motor *motor, Encoder *encoder)
 {
 	_encoder = encoder;
@@ -20,14 +23,19 @@ SpeedControl::SpeedControl(Motor *motor, Encoder *encoder)
 	_pwm = 0;
 	_lastSpeed = 0;
 
-	_kP = 1.0;
-	_kI = 1.0;
-	_kD = 1.0;
+	_kP = defaultGain;
+	_kI = defaultGain;
+	_kD = defaultGain;
 
-	_setPoint = 0;
+	_setPoint = 0; // in degrees/second
 	_minSpeed = DEFAULT_MIN_SPEED;
 }
 
+// accepts three doubles as parameters
+//   kP - the proportional gain
+//   kI - the integral gain
+//   kD - the derivative gain
+// sets the PID gains to the given values
 void SpeedControl::setGains(double kP, double kI, double kD)
 {
 	_kP = kP;
@@ -35,11 +43,20 @@ void SpeedControl::setGains(double kP, double kI, double kD)
 	_kD = kD;
 }
 
+// accepts an int as a parameter
+// sets the min speed to the given value
 void SpeedControl::setMinSpeed(int minSpeed)
 {
 	_minSpeed = minSpeed;
 }
 
+// accepts an int, speed in degrees/second, as a parameter
+// changes the set point to the abs(speed) if abs(speed) is
+// above the min speed
+// changes the set point to the min speed if abs(speed) is
+// between min speed and 0
+// sets motor direction to backward if speed is negative
+// sets motor direction to freewheel if speed is 0
 void SpeedControl::setSpeed(int speed)
 {
 	if (speed < 0)
@@ -61,11 +78,17 @@ void SpeedControl::setSpeed(int speed)
 		_setPoint = speed;
 }
 
+// returns the distance rotated by the motor's output shaft in degrees
+// since the last call to getDistance()
 int SpeedControl::getDistance()
 {
 	return _encoder->getDistance();
 }
 
+// adjusts motor's PWM level to correct for any error between the
+// set point and the actual speed of the motor's output shaft
+// MUST be called regularly on the same deltaT used to calculate
+// motor speed in the Encoder object
 void SpeedControl::adjustPWM()
 {
 	int speed = _encoder->getSpeed(); // motor control returns vector speed
